e0f0e9610d54f1038b4dfa5a52166e06cc4b15df
Waylon <waylon_zhang@qq.com>
2025-03-20 09:33:16 +0800

    feat: 添加谷歌统计

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
       keywords:
         'AI, Chat, Assistant, Mind Link, Open Source, Free, Privacy, RAG, MCP, Online, Compatible',
     },
+    tags:[
+      {tag: 'script', children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P7JDSGF9');`},
+      {tag: 'noscript', children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7JDSGF9" height="0" width="0" style="display:none;visibility:hidden"></iframe>`},
+    ]
   },
   plugins: [pluginReact(), pluginNodePolyfill(), pluginLess()],
 });
